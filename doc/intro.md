aed633975d30005c14d38f5607155005bf44bed7
Fenton Travers <fenton.travers@gmail.com>
2018-12-15 09:26:42 -0800

    .

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to elves-goblins
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/elves_goblins/core_test.clj
@@ -0,0 +1,7 @@
+(ns elves-goblins.core-test
+  (:require [clojure.test :refer :all]
+            [elves-goblins.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
