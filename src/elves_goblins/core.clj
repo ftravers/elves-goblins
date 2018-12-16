e49ed10f6f7246bc4b232b1da23f8104b735ef71
Fenton Travers <fenton.travers@gmail.com>
2018-12-15 16:34:31 -0800

    begin sorting the creatures

--- src/elves_goblins/core.clj
+++ src/elves_goblins/core.clj
@@ -6,20 +6,29 @@
   [x]
   (println x "Hello, World!"))
 
-{:game-size 3
- :creatures [{:type :elf
-              :coord [1 2]}
-             {:type :goblin
-              :coord [2 2]}
-             {:type :goblin
-              :coord [0 0]}]}
+(def un-ordered-game-state
+  {:game-size 3
+   :creatures [{:type :elf
+                :coord [1 2]}
+               {:type :goblin
+                :coord [2 2]}
+               {:type :goblin
+                :coord [0 0]}]})
+
+(def game-state-ordered
+  {:game-size 3
+   :creatures [{:type :goblin
+                :coord [0 0]}
+               {:type :elf
+                :coord [1 2]}
+               {:type :goblin
+                :coord [2 2]}]})
 
 [["#" "#" "#" "#" "#"]
  ["#" "G" "." "." "#"]
  ["#" "." "E" "." "#"]
  ["#" "." "." "G" "#"]
  ["#" "#" "#" "#" "#"]]
-
 
 (defn make-row [size char]
   (mapv (fn [x] char) (range size)))
@@ -40,6 +49,8 @@
     (into [] (cons north-wall (reverse (cons north-wall ew-walls-interior))))))
 
 (comment
+  (def creat-coords (map :coord (un-ordered-game-state :creatures)))
+  (map :coord (un-ordered-game-state :creatures))
   (def game-size 3)
   (make-row 3 "#")
   (def map3 (interior 3))
@@ -47,3 +58,44 @@
   (def north-wall (make-row 5 "#"))
   (def all-walls (add-ns-walls ew-walls-interior))
   (map println all-walls)
+  (def game-size 3)
+  (make-row 3 "#")
+  (def map3 (interior 3))
+  (def ew-walls-interior (add-ew-walls map3))
+  (def north-wall (make-row 5 "#"))
+  (def all-walls (add-ns-walls ew-walls-interior))
+  (map println all-walls))
+
+(comment
+  (def r1 1)
+  (def c1 1)
+  (def r2 2)
+  (def c2 2)
+  (sort-by
+   identity
+   (fn [[r1 c1] [r2 c2]]
+     (let [row-diff (- r1 r2)
+           col-diff (- c1 c2)]
+       (cond
+         (> row-diff 0) -1
+         (< row-diff 0) 1
+         (= row-diff)
+         (cond
+           (> col-diff 0) -1
+           (< col-diff 0) 1)))
+     '([1 2] [2 2] [0 0]))))
+
+;; => ([1 2] [2 2] [0 0]) 
+  ;; (sort-by
+  ;;  identity
+  ;;  (fn [[r1 c1] [r2 c2]]
+  ;;    (let [row-diff (- r1 r2)
+  ;;          col-diff (- c1 c2)]
+  ;;      (cond
+  ;;        (> row-diff 0) -1
+  ;;        (< row-diff 0) +1 
+  ;;        (= row-diff)
+  ;;        (cond
+  ;;          (> col-diff 0) -1
+  ;;          (< col-diff 0) 1)))
+  ;;    '([1 2] [2 2] [0 0])) 
